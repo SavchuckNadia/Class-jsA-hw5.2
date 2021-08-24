60eb4ad44016fa8aaf5b3395e1a43b1c557670c6
Nadia Savchuck <savchyk.n2003@gmail.com>
2021-08-24 17:49:27 +0300

    added the class MyString

---
+++ main.js
@@ -0,0 +1,23 @@
+class MyString {
+    reverse(string) {
+        let str1 = '';
+        for (let i = string.length - 1; i >= 0; i--) {
+            str1 += string.charAt(i);
+        }
+        return str1
+    }
+    ucFirst(string) {
+        let str1 = string[0].toUpperCase();
+        str1 += string.slice(1);
+        return str1
+    }
+    ucWords(string) {
+        let str1 = ''
+        string = string.split(' ');
+        string.forEach(element => {
+            element = element[0].toUpperCase() + element.slice(1)
+            str1 += element + ' '
+        });
+        return str1
+    }
+}
